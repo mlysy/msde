352aa5be49a8fd89d8fd90cbf14b3ad4a0ffb6bb
mlysy <mlysy@users.noreply.github.com>
2017-07-22 14:55:16 -0400

    checked Xptr speed comp.

--- tests/dontrun/test-msde_speed_comp.R
+++ tests/dontrun/test-msde_speed_comp.R
@@ -5,16 +5,16 @@
 # the new version is written with OOP and checks the speed effect of (*)
 
 require(msde) # old version
-require(msdeHeaders) # new version
-pkg.names <- c("msde", "msdeHeaders")
+require(msde2) # new version
+pkg.names <- c("msde", "msde2")
 max.diff <- function(x1, x2) {
   c(abs = max(abs(x1-x2)), rel = max(abs(x1-x2)/abs(x1)))
 }
 pkg1 <- function(fun) {
-  eval(parse(text = paste0("msde::", fun)))
+  eval(parse(text = paste0(pkg.names[1], "::", fun)))
 }
 pkg2 <- function(fun) {
-  eval(parse(text = paste0("msdeHeaders::", fun)))
+  eval(parse(text = paste0(pkg.names[2], "::", fun)))
 }
 
 # the test model is called heston's model, which is an sde with
@@ -36,18 +36,24 @@ hest.df <- function(X, Z, theta) {
 # create model objects (i.e., compile c++ code)
 
 # old version (msde)
-hmod <- pkg1("sde.make.model")(list = hestList,
-                               showOutput = TRUE, rebuild = TRUE)
+param.names <- c("alpha", "gamma", "beta", "sigma", "rho")
+data.names <- c("X", "Z")
+hmod <- pkg1("sde.make.model")
+hmod <- hmod(ModelFile = "hestModel.h", param.names = param.names,
+             data.names = data.names)
+## hmod <- pkg1("sde.make.model")(list = hestList,
+##                                showOutput = TRUE, rebuild = TRUE)
 
 # new version (msdeHeaders -- interface still in progress)
 #ndims <- 2
 #nparams <- 5
-param.names <- c("alpha", "gamma", "beta", "sigma", "rho")
-data.names <- c("X", "Z")
-hmod2 <- pkg2("sde.make.model")(ModelFile = "hestModel.h",
-                                param.names = param.names,
-                                data.names = data.names, openMP = FALSE,
-                                showOutput = TRUE, rebuild = TRUE)
+## param.names <- c("alpha", "gamma", "beta", "sigma", "rho")
+## data.names <- c("X", "Z")
+hmod2 <- pkg2("sde.make.model")
+hmod2 <- hmod2(ModelFile = "hestModel.h",
+               param.names = param.names,
+               data.names = data.names, OpenMP = FALSE,
+               showOutput = TRUE)
 ndims <- hmod2$ndims
 nparams <- hmod2$nparams
 
@@ -94,7 +100,7 @@ time.p1 <- system.time({
   df.p1 <- pkg1("sde.diff")(model = hmod, x = X0, theta = Theta)
 })
 time.p2 <- system.time({
-  dr.p2 <- pkg2("sde.drift")(model = hmod2, x = X0, theta = Theta, ncores = 8)
+  dr.p2 <- pkg2("sde.drift")(model = hmod2, x = X0, theta = Theta)
   df.p2 <- pkg2("sde.diff")(model = hmod2, x = X0, theta = Theta)
 })
 
@@ -105,8 +111,8 @@ tmp/tmp[1]
 
 #--- loglikelihood evaluations ---------------------------------------------
 
-nReps <- 100
-nObs <- 1000
+nReps <- 1000
+nObs <- 10000
 dT <- 1/252
 
 # initialize
@@ -116,19 +122,21 @@ Theta <- apply(t(replicate(nReps, theta)), 2, jitter)
 X0 <- apply(t(replicate(nReps, x0)), 2, jitter)
 
 # generate data
-hsim <- pkg1("sde.sim")(model = hmod, init.data = X0, params = Theta,
-                        dt = dT, dt.sim = dT, N = nObs, nreps = nReps)
+## hsim <- pkg1("sde.sim")(model = hmod, init.data = X0, params = Theta,
+##                         dt = dT, dt.sim = dT, N = nObs, nreps = nReps)
+hsim <- pkg1("sde.sim")
+hsim <- hsim(model = hmod, x0 = X0, theta = Theta,
+             dt = dT, dt.sim = dT, nobs = nObs, nreps = nReps)
 
 # loglikelihood calcs
 time.p1 <- system.time({
   ll.p1 <- pkg1("sde.loglik")(model = hmod, x = hsim$data, dt = dT,
                               theta = Theta)
 })
-ncores <- 8
+ncores <- 1
 time.p2 <- system.time({
-  ll.p2 <- pkg2("sde.loglik")(model = hmod2, x = aperm(hsim$data, c(2,3,1)),
-                              dt = dT, debug = FALSE,
-                              theta = Theta, ncores = ncores)
+  ll.p2 <- pkg2("sde.loglik")(model = hmod2, x = hsim$data,
+                              dt = dT, theta = Theta, ncores = ncores)
 })
 
 max.diff(ll.p1, ll.p2)
@@ -147,30 +155,34 @@ tmp/tmp[1]
 same.rnd <- TRUE
 SEED <- 3843
 
-nReps <- 2
+nReps <- 100
 theta <- c(alpha = 0.1, gamma = 1, beta = 0.8, sigma = 0.6, rho = -0.8)
 x0 <- c(X = log(1000), Z = 0.1)
 Theta <- apply(t(replicate(nReps, theta)), 2, jitter)
 X0 <- apply(t(replicate(nReps, x0)), 2, jitter)
 
 # each of these takes about 3s
-nObs <- 3
+nObs <- 1e5
 burn <- 1
 dT <- 1/252
 dt.sim <- dT/2
 if(same.rnd) set.seed(SEED)
 time.p1 <- system.time({
-  hsim.p1 <- pkg1("sde.sim")(model = hmod, init.data = X0, params = Theta,
+  ## hsim.p1 <- pkg1("sde.sim")(model = hmod, init.data = X0, params = Theta,
+  ##                            dt = dT, dt.sim = dt.sim,
+  ##                            N = nObs+1, burn = burn, nreps = nReps)
+  hsim.p1 <- pkg1("sde.sim")(model = hmod, x0 = X0, theta = Theta,
                              dt = dT, dt.sim = dt.sim,
-                             N = nObs+1, burn = burn, nreps = nReps)
+                             nobs = nObs, burn = burn, nreps = nReps)
 })
 if(same.rnd) set.seed(SEED)
 time.p2 <- system.time({
   hsim.p2 <- pkg2("sde.sim")(model = hmod2, x0 = X0, theta = Theta,
-                             dt = dT, dt.sim = dt.sim, debug = FALSE,
+                             dt = dT, dt.sim = dt.sim,
                              nobs = nObs, burn = burn, nreps = nReps)
 })
-sim1 <- aperm(hsim.p1$data, c(2,3,1))[-1,,]
+## sim1 <- aperm(hsim.p1$data, c(2,3,1))[-1,,]
+sim1 <- hsim.p1$data
 sim2 <- hsim.p2$data
 
 if(same.rnd) {
@@ -195,54 +207,52 @@ SEED <- 2531
 # simulate data
 nObs <- 1e3
 dT <- 1/252
-hsim <- pkg1("sde.sim")(model = hmod, init.data = x0, params = theta,
-                        dt = dT, dt.sim = dT/100, N = nObs, nreps = 1)
+hsim <- pkg1("sde.sim")(model = hmod, x0 = x0, theta = theta,
+                        dt = dT, dt.sim = dT/100, nobs = nObs, nreps = 1)
 
 # initialize MCMC rv's
 m <- 2 # degree of Euler approximation: dt_euler = dt/m
-par.index <- c(2, rep(1, nObs-1)) # all but first vol unobserved
-init1 <- pkg1("sde.init")(data = hsim$data, dt = dT, m = m,
-                         par.index = par.index,
-                         params = theta)
-init2 <- pkg2("sde.init")(data = hsim$data, dt = dT, m = m,
-                         par.index = par.index)
+nvar.obs <- c(2, rep(1, nObs-1)) # all but first vol unobserved
+init1 <- pkg1("sde.init")(hmod, x = hsim$data, dt = dT, m = m,
+                          nvar.obs = nvar.obs, theta = theta)
+init2 <- pkg2("sde.init")(hmod2, x = hsim$data, dt = dT, m = m,
+                          nvar.obs = nvar.obs, theta = theta)
 
 # prior
-prior <- list(Mu = c(.1, .35, 1.0, .5, -.81),
-              V = crossprod(matrix(rnorm(25),5)))
-prior$V <- sqrt(diag(c(.1, 8, .15, .002, .002))) %*% cov2cor(prior$V)
-prior$V <- prior$V %*% sqrt(diag(c(.1, 8, .15, .002, .002)))
-names(prior$Mu) <- param.names
-colnames(prior$V) <- param.names
-rownames(prior$V) <- param.names
-prior2 <- list(mu = prior$Mu, Sigma = prior$V)
+prior <- list(mu = c(.1, .35, 1.0, .5, -.81),
+              Sigma = crossprod(matrix(rnorm(25),5)))
+prior$Sigma <- sqrt(diag(c(.1, 8, .15, .002, .002))) %*% cov2cor(prior$Sigma)
+prior$Sigma <- prior$Sigma %*% sqrt(diag(c(.1, 8, .15, .002, .002)))
+names(prior$mu) <- param.names
+colnames(prior$Sigma) <- param.names
+rownames(prior$Sigma) <- param.names
+#prior2 <- list(mu = prior$Mu, Sigma = prior$V)
+prior2 <- prior
 
 # mcmc specs
 rw.jump.sd <- c(.1, 1, .1, .01, .01) # random walk metropolis for params
 names(rw.jump.sd) <- param.names
 update.params <- TRUE
 update.data <- TRUE
-nsamples <- 1e3 # ifelse(update.data, 2e4, 4e4)
+nsamples <- ifelse(update.data, 1e4, 4e4)
 burn <- 0
 
 if(same.rnd) set.seed(SEED)
 time.p1 <- system.time({
   hpost.p1 <- pkg1("sde.post")(model = hmod, init = init1,
                                nsamples = nsamples, burn = burn,
-                               prior = prior,
-                               rw.jump.sd = rw.jump.sd,
+                               hyper = prior,
+                               mwg.sd = rw.jump.sd,
                                update.params = update.params,
                                update.data = update.data)
 })
 
 if(same.rnd) set.seed(SEED)
-ncores <- 1
 time.p2 <- system.time({
-  hpost.p2 <- pkg2("sde.post")(model = hmod2, init.data = init2,
-                               init.params = theta,
+  hpost.p2 <- pkg2("sde.post")(model = hmod2, init = init2,
                                nsamples = nsamples, burn = burn,
-                               hyper.param = prior2, ncores = ncores,
-                               mwg.sd = rw.jump.sd, adapt = FALSE,
+                               hyper = prior2,
+                               mwg.sd = rw.jump.sd,
                                update.params = update.params,
                                update.data = update.data)
 })
