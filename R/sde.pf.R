7feeccccc24f89103db259e5c089b5edb238c42e
ferriszhu <ryewhisky.z@gmail.com>
2018-01-25 14:50:47 -0500

    updated sde.pf.R

--- R/sde.pf.R
+++ R/sde.pf.R
@@ -2,6 +2,7 @@
 #'
 #' @export
 sde.pf <- function(model, init, npart, Z) {
+  # model constants
   if (class(model) != "sde.model") {
     stop("model must be an sde.model object.")
   }
@@ -9,22 +10,28 @@ sde.pf <- function(model, init, npart, Z) {
   data.names <- model$data.names
   nparams <- model$nparams
   param.names <- model$param.names
-  # ndims <- 2  # for eou model
-  # nparams <- 5
   # initial values
   if (class(init) != "sde.init") {
     # all argument checking done at construction time
     stop("init must be an sde.init object.")
   }
-  # dt <- init$dt.m par.index <- init$nvar.obs.m init.data <- init$data
-  # init.params <- init$params
+  init.params <- init$params
+  init.data <- t(init$data)
+  dt <- init$dt.m
+  par.index <- init$nvar.obs.m
+
   ncomp <- nrow(init$data)
   # normal draws
   if (missing(Z))
     Z <- matrix(rnorm(npart * ndims * (ncomp - 1)), ncomp - 1, npart * ndims)
+    Z <- t(Z)
 
-  ans <- .pf_eval(sdeptr = model$ptr, initParams = init$params, initData = t(init$data),
-                dT = init$dt.m, nDimsPerObs = init$nvar.obs.m, NormalDraws = t(Z))
+  # run particle filter
+  # for matrix data type initData and NormalDraws, we use as.double() to 
+  # convert them into numeric vectors stacked by column and then pass them to C
+  ans <- .pf_eval(sdeptr = model$ptr, initParams = as.double(init$params), 
+            initData = as.double(init.data), dT = as.double(dt),
+            nDimsPerObs = as.integer(par.index), NormalDraws = as.double(Z))
   ans$X <- t(ans$X)
   ans$lwgt <- t(ans$lwgt)
   return(ans)
@@ -37,5 +44,9 @@ sde.pf <- function(model, init, npart, Z) {
 #     - sdeSMC.cpp should become (possible multiple) header files.
 # 2.  add smc debug to each model using msde-test_debug.R
 #
-# 1) I need to change sdeInterface.h
+# my steps:
+# 1) change sdeInterface.h accordingly
 # 2) translate particleEval to a function template and include it in a header file (similar as sdeSim.h/sdePost.h)
+# 3) modify the function parameter list of particleEval, just as sdeSim.h & sdePost.h
+# 4) add particleEval in sdeExports.cpp
+# 5) change eou.pf.R into sde.pf.R and update the function body (refer to sde.sim.R & sde.post.R)
