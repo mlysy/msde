1f0c0a589ad89bd1cec88da2ce914137abaf4014
ferriszhu <ryewhisky.z@gmail.com>
2018-03-20 23:03:43 -0400

    modified the adaptive RW proposal for sde.pmcmc

--- vignettes/sde.pmcmc.R
+++ vignettes/sde.pmcmc.R
@@ -9,25 +9,26 @@
 # @param dT Scalar interobservation time.
 # @param resample The type of particle resampling scheme to use. These are: multi(nomial), resid(ual), strat(ified), sys(tematic).
 # @param threshold A scalar less than 1 to indicate the threshold for resampling. A negative number disables resampling.
-# @param mwg.sd Standard deviation jump size for Metropolis-within-Gibbs on parameters and missing components of first SDE observation.
+# @param rw.sd Standard deviation jump size for random walk proposal step size on parameters and missing components of first SDE observation.
+# @param delta Tuning parameter for adaptive rw.sd. It has the same length as rw.sd
 #
 # @return A list of the following elements:
 # \describe{
 #   \item{\code{params}}{An \code{nsamples x nparams} matrix of posterior parameter draws.}
 #   \item{\code{accept}}{A named list of acceptance rates for the various components of the MCMC sampler.}
 #   \item{\code{init}}{The \code{sde.init} object which initialized the sampler.}
-#   \item{\code{mwg.sd}}{A named vector of Metropolis-within-Gibbs standard devations used at the last posterior iteration.}
+#   \item{\code{rw.sd}}{A named vector of RW standard devations used at the last posterior iteration.}
+#   \item{\code{delta}}{A numeric value or a vector of tuning parameter(s) for rw.sd}
 # }
 #
 sde.pmcmc <- function(model, init, theta0, fixed.params, 
                       nsamples, npart, dT,
-                      resample = "multi", threshold = 0.5, mwg.sd = 1) {
+                      resample = "multi", threshold = 0.5, rw.sd = 1, delta = .618) {
   # iteration = 0, initialize the sde model for PMCMC
   # get initial log-weights & log marginal likelihood via particle filtering
   pf <- sde.pf(model, init, npart, resample, threshold, history = FALSE)
   lwgt <- pf$lwgt
   logYt <- .logY(lwgt, npart)
-  
   # posterior sampling
   M <- nsamples
   nParams <- length(theta0)
@@ -39,14 +40,23 @@ sde.pmcmc <- function(model, init, theta0, fixed.params,
   for(ii in 2:(M+1)) {
     theta_old <- thetaMatrix[ii-1, ]
     # random walk proposal for theta
+    # adaptive sd, increase sd if the last draw was accepted; otherwise, decrease sd
+    # note ii starts from 2, so the current iteration is the (ii-1)th iteration
+    if(ii-1 == 1) {
+      rw.sd <- rw.sd
+    } else if(ii-1 > 1) {
+      if(acc[(ii-1)-1] == TRUE) {
+        rw.sd <- exp(log(rw.sd) + delta/ii)
+      } else {
+        rw.sd <- exp(log(rw.sd) - delta/ii)
+      }
+    }
     # only change the unknown (non-fixed) Lambda1
     sd <- rep(NA, nParams)
     sd[1:nParams][fixed.params] <- 0
-    sd[1:nParams][!fixed.params] <- mwg.sd
+    sd[1:nParams][!fixed.params] <- rw.sd
     theta_prop <- rnorm(nParams, mean = theta_old, sd = sd)
-    
     names(theta_prop) <- model$param.names
-    
     # run the particle filter based on theta_prop
     # assume no artificial missing points placed between observations, m = 1 by default
     tmp_init <- sde.init(model, x = YObs, dt = dT, theta = theta_prop, nvar.obs = 1)
@@ -55,27 +65,25 @@ sde.pmcmc <- function(model, init, theta0, fixed.params,
     # calculate log p_theta_prop(y_T) 
     lwgt <- tmp_pf$lwgt
     logYt_prop <- .logY(lwgt, npart)
-    
     # calculate the log acceptance ratio
     # remember we use log density 
     # we have assumed prior of theta to be 1 for simplicity
     logRatio <- logYt_prop - logYt + 
       sum(dnorm(theta_old[!fixed.params], mean = theta_prop[!fixed.params], sd = sd[!fixed.params], log = TRUE)) -
       sum(dnorm(theta_prop[!fixed.params], mean = theta_old[!fixed.params], sd = sd[!fixed.params], log = TRUE))
-    
     if (logRatio > log(runif(1))) {
       # accept the proposal
       thetaMatrix[ii, ] <- theta_prop
       logYt <- logYt_prop
-      acc[ii-1] <- 1
+      acc[ii-1] <- TRUE
     } else {
       thetaMatrix[ii, ] <- theta_old
       #logYT <- logYT
-      acc[ii-1] <- 0
+      acc[ii-1] <- FALSE
     }
   }
   accRate <- sum(acc)/M
-  out <- list(params = thetaMatrix, accept = accRate, init = init, mwg.sd = mwg.sd)
+  out <- list(params = thetaMatrix, accept = accRate, init = init, rw.sd = rw.sd)
   return(out)
 }
 
